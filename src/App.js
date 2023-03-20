22103032a51944a9cfd2fe9e77f638e7ac1189e0
BushraFatimaBF <vglampard@protonmail.com>
2023-03-20 19:05:17 +0000

    neatened up styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
 import { NostrProvider } from "nostr-react";
-// import { GlobalFeed } from './components/TextEvents/TextEvents';
-import SpecificUserEvents from './components/SpecificUserEvents/SpecificUserEvents';
+import { GlobalFeed } from './components/TextEvents/TextEvents';
+// import SpecificUserEvents from './components/SpecificUserEvents/SpecificUserEvents';
 const relayUrls = [
   // "wss://nostr-pub.wellorder.net",
   // "wss://relay.nostr.ch",
-  "wss://relay.damus.io",
+  // "wss://relay.damus.io",
+  "wss://relay.nostr.band/trusted"
 ];
 
 function App() {
   return (
     <NostrProvider relayUrls={relayUrls} debug={true}>
     <div className="App">
-      <header className="App-header">
-        <p> nostr</p>
+      <header >
+       <div className="hero">
+        <h1> Nostrami</h1>
+        </div>
+        <div className="feed-container">
         {/* <SpecificUserEvents/> */}
-        {/* <GlobalFeed/> */}
+        <GlobalFeed/>
+        </div>
       </header>
-   
+  
     </div>
     </NostrProvider>
   );
