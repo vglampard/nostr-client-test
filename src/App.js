60b265faa2951ea05b5b86d10ccae13a51bbf527
BushraFatimaBF <vglampard@protonmail.com>
2023-03-20 22:56:52 +0000

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { NostrProvider } from "nostr-react";
 import { GlobalFeed } from "./components/TextEvents/TextEvents";
+import Navbar from "./components/Navbar/Navbar";
 // import SpecificUserEvents from './components/SpecificUserEvents/SpecificUserEvents';
 const relayUrls = [
   // "wss://nostr-pub.wellorder.net",
@@ -13,11 +14,9 @@ function App() {
   return (
     <NostrProvider relayUrls={relayUrls} debug={true}>
       <div className="App">
+        <Navbar/>
         <header>
-          <div className="hero">
-            <h1> Nostrami</h1>
-          
-          </div>
+
           <div className="feed-container">
             {/* <SpecificUserEvents/> */}
             <GlobalFeed />
