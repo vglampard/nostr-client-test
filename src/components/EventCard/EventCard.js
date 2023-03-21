20da1f65631d1a317b5d660cdd7b29379424e1ae
BushraFatimaBF <vglampard@protonmail.com>
2023-03-21 00:17:05 +0000

    added image previews on tiles

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import LongformModal from '../LongformModal/LongformModal'
-
-import "./textCard.css"
-export default function EventCard({event}) {
-  let substring = event.content.substring(0,240)+"..."
-  console.log("SUBSTRING:", substring)
+import React from "react";
+import LongformModal from "../LongformModal/LongformModal";
+import "./textCard.css";
+function getImage(event) {
+  let img = event.filter((tag) => tag[0] === "image");
+  return img;
+}
+export default function EventCard({ event }) {
+  let substring = event.content.substring(0, 240) + "...";
+  let img = getImage(event.tags);
   return (
     <div className="textCard">
-        <p>{substring}</p>
-        <LongformModal event={event}/>
-        {event.tags[5] && <img src={event.tags[5][1]} alt={event.tags[0][1]}/>}
+      <p>{substring}</p>
+      <LongformModal event={event} />
+      {img.length !== 0 && <img src={img[0][1]} alt={event.tags[0][1]} />}
     </div>
-  )
+  );
 }
