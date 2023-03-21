07e66c4bfd75646a9685c6dee3f2b359c88521aa
BushraFatimaBF <vglampard@protonmail.com>
2023-03-21 00:24:48 +0000

    tweaked get requestso not pulling too much data

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,15 +1,18 @@
 import React from "react";
 import LongformModal from "../LongformModal/LongformModal";
 import "./textCard.css";
+
 function getImage(event) {
   let img = event.filter((tag) => tag[0] === "image");
   return img;
 }
+
 export default function EventCard({ event }) {
   let substring = event.content.substring(0, 240) + "...";
   let img = getImage(event.tags);
   return (
     <div className="textCard">
+      <h1>{event.tags[1][1]}</h1>
       <p>{substring}</p>
       <LongformModal event={event} />
       {img.length !== 0 && <img src={img[0][1]} alt={event.tags[0][1]} />}
--- src/components/LongformContent/LongformContent.js
+++ src/components/LongformContent/LongformContent.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { useNostrEvents } from "nostr-react";
+import { useNostrEvents, dateToUnix  } from "nostr-react";
 import EventCard from '../EventCard/EventCard';
+import { useRef } from "react";
 
+// pulling long form from a set time a while ago so it's bearable to style on
 export default function LongformContent() {
+    const now = useRef(new Date()); // Make sure current time isn't re-rendered
 
     const { events } = useNostrEvents({
         filter: {
-          since: 0,
-        //   kinds: [30023],
+          since: dateToUnix(now.current)-10000,
+          kinds: [30023],
         },
       });
  
