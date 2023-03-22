2168bde73aef1315d95d4063ac349f9fb3b13dba
BushraFatimaBF <vglampard@protonmail.com>
2023-03-22 14:32:43 +0000

    added toats popups instead of alert

--- src/components/Translate/Translate.js
+++ src/components/Translate/Translate.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import rightArrow from "./right-arrow.png"
+import React from "react";
+import rightArrow from "./right-arrow.png";
 
 export default function Translate() {
   return (
-    <div >
-        <p>Limited feature: first 5,000 characters copied to clipboard, jump over to DeepL to get best translation </p>
-      <a href="https://www.deepl.com/translator"><img
-            src={rightArrow}
-            alt="arrow that takes usesr to DeepL translator when clicked"
-          /></a>
-
-</div>
-      
-  )
+    <div className="flex space-x-4">
+      <p className="flex-auto">
+        Limited feature: first 5,000 characters copied to clipboard, jump over
+        to DeepL to get best translation{" "}
+      </p>
+      <a
+        href="https://www.deepl.com/translator"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={rightArrow}
+          alt="arrow that takes user to DeepL translator when clicked"
+          className="h-[50%]"
+        />
+      </a>
+    </div>
+  );
 }
