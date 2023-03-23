2bd1167e1dbdca23100ed2b174cadfe997d34744
BushraFatimaBF <vglampard@protonmail.com>
2023-03-23 16:27:27 +0000

    styling continues

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-
+import seeMore from "./more.png"
 import ReactMarkdown from "react-markdown";
 import userIcon from "./user.png";
 import {
@@ -12,7 +12,7 @@ import {
 
 export default function EventCard({ post, modalStates }) {
   let substring = post.content.substring(0, 100) + "...";
-  let [seeMore, setSeeMore] = useState(false)
+  let [seeMore, setSeeMore] = useState(false);
 
   let img = getImage(post.tags);
   let title = getTitle(post.tags);
@@ -21,50 +21,55 @@ export default function EventCard({ post, modalStates }) {
 
   return (
     <div className="bg-slate-100 bg-opacity-50 rounded break-inside-avoid drop-shadow-xl text-xs flex flex-col h-auto w-full aspect-video mb-6  ">
-      <h1 className="font-bold pt-1 leading-1">{title}</h1>
-      <p className="text-xs">{date}</p>
-   
-      {img.length !== 0 && (
-        <img
-          src={img[0][1]}
-          alt={post.tags[0][1]}
-          className="w-[1oo%]"
-        />
-      )}
+      <h1 className="font-bold pt-1 leading-1 ml-1">{title}</h1>
+      <p className="text-xs ml-1">{date}</p>
 
-      <div className="flex gap-1 flex-wrap justify-start">
-        {tags.map((tag) => {
-          return <p className="bg-orange-50 rounded-lg ">#{tag[1]}</p>;
-        })}
+      {img.length !== 0 && (
+        <img src={img[0][1]} alt={post.tags[0][1]} className="w-[1oo%]" />
+      )}
+<div className="flex">
+      <div className="flex justify-start flex-wrap leading-tight ml-1">
+          {tags.map((tag) => {
+            return (
+              <p className="bg-slate-50 bg-opacity-20 rounded-lg text-[0.65rem] mt-1 px-1 h-5">#{tag[1]}</p>
+            );
+          })}
+        </div>
+        <div className="">
+          <button
+            className="bg-slate-200 bg-opacity-30 text-white active:bg-slate-600 font-bold uppercase text-sm rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150 align-right w-5 pb-1 px-3 flex content-center justify-center m-2"
+            onClick={() => {
+              modalStates.setModalEvent(post);
+              setSeeMore(!seeMore);
+            }}
+          >
+           +
+          </button>
       </div>
-      <button  className="bg-slate-200 bg-opacity-30 text-white active:bg-slate-600 font-bold uppercase text-sm  py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-2 mb-2 ease-linear transition-all duration-150 align-right w-9"
-              onClick={() => {
-                modalStates.setModalEvent(post); setSeeMore(!seeMore);
-              }}
-            >
-              . . .
-            </button>
-{seeMore && <div className="bg-white bg-opacity-40 rounded-lg">
-        <div className=" break-words w-[100%] items-center p-2">
-          <img src={userIcon} alt="user icon" className="w-4 h-4" />
-          <p className="break-words w-[100%]">{post.pubkey}</p>
-          <div className="grid justify-items-end">
-              <ReactMarkdown className="flex flex-wrap break-all text-xs p-2">
-        {substring}
-      </ReactMarkdown>
-            <button
-              className=""
-              type="button"
-              onClick={() => {
-                modalStates.setShowModal(true);
-              }}
-            >
-              Read full...
-            </button>
+      </div>
+      {seeMore && (
+        <div className="bg-white bg-opacity-40 rounded-lg">
+          <div className=" flex break-words w-[100%] items-center p-2">
+            <img src={userIcon} alt="user icon" className="w-4 h-4" />
+            <p className="break-words w-[100%]">{post.pubkey}</p>
+            </div>
+            <div className="grid justify-items-end">
+              <ReactMarkdown className="flex flex-wrap break-all text-xs px-2 ">
+                {substring}
+              </ReactMarkdown>
+              <button
+                className="p-2"
+                type="button"
+                onClick={() => {
+                  modalStates.setShowModal(true);
+                }}
+              >
+                Read full...
+              </button>
+           
           </div>
         </div>
-      </div>}
-      
+      )}
     </div>
   );
 }
--- src/components/LongformContent/LongformContent.js
+++ src/components/LongformContent/LongformContent.js
@@ -4,24 +4,29 @@ import EventCard from "../EventCard/EventCard";
 import { useRef } from "react";
 import { getImage, checkImage } from "../../helpers/helpers";
 
-export default function LongformContent({modalStates}) {
+export default function LongformContent({ modalStates }) {
+
+  //Get all longform events from 600000 posts ago (starting now)
   const now = useRef(new Date()); // Make sure current time isn't re-rendered
   const { events } = useNostrEvents({
     filter: {
-      since: dateToUnix(now.current) - 350000,
+      since: dateToUnix(now.current) - 600000,
       kinds: [30023],
     },
   });
 
-const imageEvents = events.filter((event)=>checkImage(getImage(event.tags)));
-  
-  return (
-    <div class="gap-3 columns-1 md:columns-3 lg:columns-4%">
+  // Filter out any long form posts without image URLs that can be displayed (filters out file names and anything from Bucketeer)
+  const imageEvents = events.filter((event) =>
+    checkImage(getImage(event.tags))
+  );
 
-{imageEvents.map((post) => (
+  return (
+
+    // For each post, render one card with ability to pass its own info up to the modal component for display
+    <div class="gap-3 columns-1 md:columns-3 lg:columns-4 md:px-5 px-2">
+      {imageEvents.map((post) => (
         <EventCard post={post} modalStates={modalStates} />
       ))}
-
-</div>
+    </div>
   );
 }
